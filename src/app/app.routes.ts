af1557146e32582a7ee4e042f76bbf76f4edcbef
IPdotSetAF <ipdotsetaf@gmail.com>
2024-10-22 21:28:44 +0330

    made textareas code-y

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ export const routes: Routes = [
     { path: '', redirectTo: "home", pathMatch: "full" },
 
     { path: 'home', title: "CodeChef", component: HomeComponent },
-    { path: 'cs2ts', title: "C# 2 TS • CodeChef", component: Cs2tsComponent },
+    { path: 'cs2ts', title: "C# to TS • CodeChef", component: Cs2tsComponent },
 
     { path: '**', title: "Not Found • CodeChef", component: NotFoundComponent },
 ];
