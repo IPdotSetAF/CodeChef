9f49767317940bbdb2fc9218baec6d93ec32be33
IPdotSetAF <ipdotsetaf@gmail.com>
2024-10-22 18:18:43 +0330

    created cs2ts page

--- src/app/cs2ts/cs2ts.component.ts
+++ src/app/cs2ts/cs2ts.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { debounceTime, Subject, takeLast, tap } from 'rxjs';
 
 @Component({
   selector: 'app-cs2ts',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cs2ts.component.html',
   styleUrl: './cs2ts.component.css'
 })
-export class Cs2tsComponent {
+export class Cs2tsComponent implements AfterContentInit {
+  protected csModel !: string;
+  protected tsModel !: string;
+
+  protected inputDebouncer = new Subject<string>();
+
+  ngAfterContentInit(): void {
+    this.inputDebouncer.pipe(
+      debounceTime(500),
+      tap(value => this.convert(value)),
+      takeLast(1),
+    ).subscribe();
+  }
+
+  protected convert(csCode?: string) {
+    const value = csCode ? csCode : this.csModel;
+
+    this.tsModel = value;
+  }
 }
