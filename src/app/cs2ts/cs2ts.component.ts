26fa72cda3605f6b7281b5c3ccd12859af368bb9
IPdotSetAF <ipdotsetaf@gmail.com>
2024-10-22 19:33:17 +0330

    added cs2ts regex

--- src/app/cs2ts/cs2ts.component.ts
+++ src/app/cs2ts/cs2ts.component.ts
@@ -1,5 +1,6 @@
 import { AfterContentInit, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { log } from 'console';
 import { debounceTime, Subject, takeLast, tap } from 'rxjs';
 
 @Component({
@@ -13,6 +14,9 @@ export class Cs2tsComponent implements AfterContentInit {
   protected csModel !: string;
   protected tsModel !: string;
 
+  protected regex !: RegExp;
+  protected replace !: string;
+
   protected inputDebouncer = new Subject<string>();
 
   ngAfterContentInit(): void {
@@ -24,8 +28,8 @@ export class Cs2tsComponent implements AfterContentInit {
   }
 
   protected convert(csCode?: string) {
-    const value = csCode ? csCode : this.csModel;
-
-    this.tsModel = value;
+    let value = csCode ? csCode : this.csModel;
+    //$<name> : $<gen:+$<gen><<$<num:+number:>$<str>$<bool:+boolean:>$<type>$<arr2:+[]>>>:$<num:+number:>$<str>$<bool:+boolean:>$<type>$<arr2:+[]>>$<arr1:+[]>$<arr3:+[]> ;
+    this.tsModel = value.replaceAll(/public ((?<arr1>IEnumerable<|List<|ICollection<)|(?<gen>\w+)<)*((?<num>int|float|double|decimal|long)|(?<str>string)|(?<bool>bool)|(?<type>\w+))(?<arr2>\[\])?(>)?(?<arr3>\[\])? (?<name>\w+) (.+)/gm, this.replace);
   }
 }
