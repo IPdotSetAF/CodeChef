6d2716a08d2904fbdff5acdda1e1cde234cddc80
IPdotSetAF <ipdotsetaf@gmail.com>
2024-10-23 19:11:22 +0330

    moved styles added change event to code

--- src/app/code-area/code-area.component.ts
+++ src/app/code-area/code-area.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, HostListener, input, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Input, Output, EventEmitter } from '@angular/core';
 import Prism from 'prismjs';
 import { debounceTime, Subject, takeLast, tap } from 'rxjs';
 
@@ -13,32 +13,37 @@ export class CodeAreaComponent implements AfterViewInit {
   @Input()
   code!: string;
 
+  @Output()
+  codeChange = new EventEmitter<string>();
+
   @Input()
   language = 'javascript';
 
   constructor(private el: ElementRef) { }
 
   ngAfterViewInit() {
-    this.highlight(this.code || this.el.nativeElement.innerText)
+    this.highlight(this.code || this.el.nativeElement.value)
 
     this.inputDebouncer.pipe(
       debounceTime(500),
       tap(value => this.highlight(value)),
       takeLast(1),
     ).subscribe();
-  }
-  
-  private inputDebouncer = new Subject<string>();
-  
-  @HostListener("input")
-  protected onInput(){
-    this.inputDebouncer.next(this.el.nativeElement.innerText);
+
+    this.codeChange.subscribe(v => this.highlight(v));
   }
 
-  private highlight(code: string){
+  private inputDebouncer = new Subject<string>();
+
+  @HostListener("input")
+  protected onInput() {
+    this.inputDebouncer.next(this.el.nativeElement.value);
+  }
+
+  private highlight(code: string) {
     const grammar = Prism.languages[this.language];
     const html = Prism.highlight(code, grammar, this.language);
     this.el.nativeElement.innerHTML = html;
-    console.log(code);
+    console.log(html);
   }
 }
