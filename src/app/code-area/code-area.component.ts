110befc2865f4f1cec901e8b10c06fb73a0af7b9
IPdotSetAF <ipdotsetaf@gmail.com>
2024-10-25 23:09:31 +0330

    removed prism implementatioon implemented prism-code-editor (does not run)

--- src/app/code-area/code-area.component.ts
+++ src/app/code-area/code-area.component.ts
@@ -1,46 +1,98 @@
-import { AfterViewInit, Component, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
-import Prism from 'prismjs';
-import 'prismjs/plugins/line-numbers/prism-line-numbers';
-import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
-import 'prismjs/components/prism-csharp';
-import 'prismjs/plugins/toolbar/prism-toolbar';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  input,
+  model,
+  OnChanges,
+  PLATFORM_ID,
+  SimpleChanges,
+  ViewChild,
+  ViewEncapsulation,
+} from "@angular/core";
+import { createEditor, PrismEditor } from "prism-code-editor";
+import { matchBrackets } from "prism-code-editor/match-brackets";
+import { highlightBracketPairs } from "prism-code-editor/highlight-brackets";
+import { editHistory } from "prism-code-editor/commands";
+import { copyButton } from "prism-code-editor/copy-button";
+import "prism-code-editor/prism/languages/json";
+import "prism-code-editor/prism/languages/yaml";
+import "prism-code-editor/prism/languages/http";
+import "prism-code-editor/prism/languages/java";
+import "prism-code-editor/prism/languages/kotlin";
+import "prism-code-editor/prism/languages/markdown";
+import "prism-code-editor/prism/languages/xml";
+import { isPlatformBrowser } from "@angular/common";
+import { noop } from "rxjs";
 
 @Component({
   selector: 'code-area',
   standalone: true,
-  imports: [FormsModule],
-  templateUrl: './code-area.component.html',
-  styleUrl: './code-area.component.css',
-  encapsulation: ViewEncapsulation.None,
+  imports: [],
+  template: "<div #editorContainer></div>",
+  styles: [
+    `
+      :host > div {
+        margin-bottom: 0.5em;
+      }
+    `,
+  ],
+  encapsulation: ViewEncapsulation.None
 })
-export class CodeAreaComponent implements AfterViewInit {
-  @Input() public code!: string;
-  @Output() public codeChange = new EventEmitter<string>();
+export class CodeAreaComponent implements AfterViewInit, OnChanges {
+  code = model<string>("");
+  language = input<string>("markdown");
+  readonly = input<boolean, string>(false, {
+    transform: (value: string) => value == "true",
+  });
+  editor: PrismEditor | undefined = undefined;
 
-  @Input() public placeholder!: string;
-  @Input() public language = 'csharp';
+  @ViewChild("editorContainer") editorContainer!: ElementRef;
 
-  protected codeHtml!: string;
-  private grammar!: Prism.Grammar;
-
-  constructor() { }
-
-  ngAfterViewInit() {
-    this.grammar = Prism.languages[this.language];
-
-    if (this.code)
-      this.highlight(this.code)
+  isBrowser = false;
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
   }
 
-  protected onInput() {
-    this.codeHtml = this.highlight(this.code);
-    Prism.highlightAll();
-    this.codeChange.emit(this.code);
+  ngAfterViewInit(): void {
+    if (!this.isBrowser)
+      return;
+
+    if (this.editorContainer.nativeElement) {
+      this.editor = this.initEditor();
+    }
   }
 
-  private highlight(code: string): string {
-    return Prism.highlight(code, this.grammar, this.language);
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes["code"].previousValue !== changes["code"].currentValue) {
+      this.editor?.setOptions({ value: changes["code"].currentValue });
+    }
+  }
+
+  initEditor(): PrismEditor {
+    const editor = createEditor(this.editorContainer.nativeElement, {
+      value: this.code(),
+      language: this.language(),
+      lineNumbers: false,
+      wordWrap: true,
+      readOnly: this.readonly(),
+
+      onUpdate: (code: string) => {
+        this.code.set(code);
+      },
+    });
+    editor.addExtensions(
+      copyButton(),
+      matchBrackets(true),
+      highlightBracketPairs(),
+      editHistory()
+    );
+
+    this.code.subscribe(() => {
+      this.editor?.update();
+    });
+
+    return editor;
   }
 }
