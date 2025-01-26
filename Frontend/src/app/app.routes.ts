7a07eed3dbba2c274fc9ba106e1481857a185461
IPdotSetAF <ipdotsetaf@gmail.com>
2025-01-27 00:10:10 +0330

    added id generator page

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { Md2htmlComponent } from './md2html/md2html.component';
 import { SerializedToolComponent } from './serialized-tool/serialized-tool.component';
 import { MssqlScaffolderComponent } from './mssql-scaffolder/mssql-scaffolder.component';
 import { DownloadsComponent } from './downloads/downloads.component';
+import { IdGeneratorComponent } from './id-generator/id-generator.component';
 
 export const routes: Routes = [
     { path: '', title: "CodeChef", component: HomeComponent, data: { header: false } },
@@ -13,6 +14,7 @@ export const routes: Routes = [
     { path: 'md2html', title: "MD to HTML • CodeChef", component: Md2htmlComponent },
     { path: 'serialized', title: "Serialized Tool • CodeChef", component: SerializedToolComponent },
     { path: 'mssqlscaffold', title: "MSSQL Scaffolder • CodeChef", component: MssqlScaffolderComponent },
+    { path: 'id', title: "ID Generator • CodeChef", component: IdGeneratorComponent },
     { path: 'downloads', title: "Downloads • CodeChef", component: DownloadsComponent },
 
     { path: '**', title: "Not Found • CodeChef", component: NotFoundComponent },
